27e349a1ce5e2a30d7cd91fab9fa7d8533649f09
Barry Kui <barrykui@users.noreply.github.com>
2016-09-10 16:29:23 +0800

    Create xparallel.sh

---
+++ xparallel.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#### User Code Region###
+faDir=$1
+
+# according to your computer
+SEND_THREAD_NUM=60
+#######################
+
+tmp_fifofile="/tmp/$$.fifo"
+mkfifo "$tmp_fifofile"
+exec 6<>"$tmp_fifofile"
+for ((i=0;i<$SEND_THREAD_NUM;i++));do
+                 echo                                                                                    
+done >&6
+
+
+#### User Code Region 2 ###
+for file in $faDir/*.fa
+###########################
+do
+        read -u6
+        {
+                #### User Code Region 3 ###
+                ps aux|grep  python
+                ###########################
+                sleep 1
+                echo >&6
+        } &
+
+
+                pid=$!
+                echo $pid
+
+done
+
+wait
+exec 6>&-
+exit 0
